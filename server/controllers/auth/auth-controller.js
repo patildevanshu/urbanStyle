d1f2de64490e69af08c4ef2edb951195c21941e0
DEVANSHU PATIL <devanshupatil34@gmail.com>
2026-09-10 01:36:14 +0530

    feat(auth): support role parameter in user registration

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -4,7 +4,7 @@ const User = require("../../models/User");
 
 //register
 const registerUser = async (req, res) => {
-  const { userName, email, password } = req.body;
+  const { userName, email, password, role } = req.body;
 
   try {
     const checkUser = await User.findOne({ email });
@@ -19,6 +19,7 @@ const registerUser = async (req, res) => {
       userName,
       email,
       password: hashPassword,
+      role: role || "user",
     });
 
     await newUser.save();
